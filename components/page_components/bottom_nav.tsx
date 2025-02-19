804ec1a7010dda68a948b71d4dddeb8548f0d2ef
Justforvan <stephanus@justforvan.com>
2025-02-19 14:28:08 +0700

    Modifying the bottom nav

--- components/page_components/bottom_nav.tsx
+++ components/page_components/bottom_nav.tsx
@@ -8,20 +8,27 @@ export default function BottomNav() {
   const pathname = usePathname();
 
   const navItems = [
-    { href: "/", icon: Home },
-    { href: "/history", icon: History },
-    { href: "/account", icon: User },
+    { href: "/", label: "Home", icon: Home },
+    { href: "/history", label: "History", icon: History },
+    { href: "/account", label: "Account", icon: User },
   ];
 
   return (
-    <div className="w-full grid grid-cols-3 bg-blue-800 text-white p-6 text-center fixed bottom-0">
-      {navItems.map(({ href, icon: Icon }) => (
+    <div className="w-full grid grid-cols-3 text-black border border-gray-300 p-3 text-center fixed bottom-0">
+      {navItems.map(({ href, label, icon: Icon }) => (
         <Link key={href} href={href} className="flex flex-col items-center">
           <Icon
             className={`text-center ${
-              pathname === href ? "text-yellow-400" : "text-white"
+              pathname === href ? "text-yellow-600" : ""
             }`}
           />
+          <span
+            className={`text-center text-sm ${
+              pathname === href ? "text-yellow-600" : ""
+            }`}
+          >
+            {label}
+          </span>
         </Link>
       ))}
     </div>
